b89cb87148ab7983f8c9494ef3f7b7ac4bda28b1
KMW <dndaks1304@gmail.com>
2019-12-30 18:13:54 +0900

    배고픈 저녁

--- Python/Project Euler/Practice.py
+++ Python/Project Euler/Practice.py
@@ -1,24 +1,11 @@
-star = ["***","* *","***"]
+N,M = list(map(int,input().split()))
+arr = list(map(int,input().split()))
 
-n = int(input())
-
-def get_star(n):
-    mat = []
-    l = len(n)
-    for i in range(l * 3):
-        if i // 3 == 1:
-            mat.append(n[i % l] + " " * l + n[i % l])
-        else:
-            mat.append(n[i % l] * 3)
-    return list(mat)
-
-k = 0
-while n != 3:
-    n = n // 3
-    k += 1
-
-for i in range(k):
-    star = get_star(star)
-
-for i in star:
-    print(i)
+res = sum(arr)
+for i in range(len(arr) - 2):
+    for j in range(i + 1,len(arr) - 1):
+        for k in range(j + 1,len(arr)):
+            sum = arr[i] + arr[j] + arr[k]
+            if sum <= M and M - sum < M - res:
+                res = sum
+print(res)

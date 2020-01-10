34ab2709a4213e767ef3bbdaa7c59c3a7dadc322
KMW <dndaks1304@gmail.com>
2020-01-10 19:10:34 +0900

    파싱

--- CPP/채널 부스팅 이벤트(가위바위보)/Main/Main/Main.cpp
+++ CPP/채널 부스팅 이벤트(가위바위보)/Main/Main/Main.cpp
@@ -4,7 +4,14 @@
 
 int main()
 {
-	shared_ptr<CMainGUI> pMainGUI = make_shared<CMainGUI>();
+	//shared_ptr<CMainGUI> pMainGUI = make_shared<CMainGUI>();
+
+	map<int,int> a = { 1,2,3,4,5 };
+	vector<int> b = { 1,2,3};
+
+	copy(begin(a), end(a), begin(b));
+
+	for_each(begin(b), end(b), [&](int x) {cout << x << endl; });
 
 	return 0;
 }
--- Python/Project Euler/Practice.py
+++ Python/Project Euler/Practice.py
@@ -1,17 +1,33 @@
-n = int(input())
+N,M = map(int,input().split())
+data = [ i + 1 for i in range(N)]
+check = [False] * N
 arr = []
-for _ in range(n):
-    arr.append(list(map(int,input().split())))
 
-len = len(arr)
-rank = [1] * len
+def DFS(n):
+    # 주어진 갯수만큼 채워지면 출력한다.
+    if n == M:
+        print(*arr)
+        return
 
-for i in range(len):
-    for x in range(len):
-        if i == x:
+    # data 갯수만큼 순회한다.
+    for i in range(N):
+        # 이미 거쳐간 수라면 그냥 지나간다.
+        if check[i]:
             continue
-        if arr[i][0] < arr[x][0] and arr[i][1] < arr[x][1]:
-            rank[i]+=1
 
-for i in rank:
-    print(i,end=' ')
+        # i번째는 거쳐갈거라서 True로 바꾼다.
+        check[i] = True
+
+        # 데이터를 넣는다.
+        arr.append(data[i])
+
+        # 현재의 i를 기준으로 가지치기를 시작
+        DFS(n+1)
+
+        # 최근에 넣은 데이터 하나 뺀다.
+        arr.pop()
+
+        # 거쳐갔다는 걸 취소한다.
+        check[i] = False
+
+DFS(0)

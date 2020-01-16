ab05afa2f5fb3f9fd071b2410b9215fe27d637b7
KMW <dndaks1304@gmail.com>
2020-01-16 21:23:07 +0900

    열정!

---
+++ CPP/TestCPP/TestCPP/200116.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+auto main()->int
+{
+	vector<int> vec = { 1,2,3,4,5 };
+
+	//vec.erase(remove(begin(vec), end(vec),), end(vec));
+
+	return 0;
+}

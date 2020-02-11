96e9d5042d01a696606881f3b0029b6b44a7a49d
KMW <dndaks1304@gmail.com>
2020-02-11 17:58:37 +0900

    책..?!!

---
+++ Python/ProblemBank/200211_3.py
@@ -0,0 +1,16 @@
+# 041
+movie_rank = ["닥터 스트레인지","스플릿","럭키"]
+
+# 042
+movie_rank.append("배트맨")
+
+# 043
+movie_rank.insert(1,"슈퍼맨")
+
+# 044
+movie_rank.remove("럭키")
+
+# 045
+movie_rank.remove("스플릿")
+movie_rank.remove("배트맨")
+
